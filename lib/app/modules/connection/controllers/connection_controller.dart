2eb5086490da9970dce6e6797841617dfe077bdc
ivannn132 <ivanprayogaadi@gmail.com>
2024-12-14 22:33:10 +0700

    delete unused import

--- lib/app/modules/connection/controllers/connection_controller.dart
+++ lib/app/modules/connection/controllers/connection_controller.dart
@@ -1,8 +1,6 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sewa/app/modules/home/views/homepage_view.dart';
-import 'package:sewa/app/modules/login/views/login_view.dart';
 
 import '../views/no_connection_view.dart';
 
